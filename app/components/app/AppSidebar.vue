a1e257b63a676bc722bf40c8331a366da7ac7b20
Mark AI <chanthavong_12@hotmail.com>
2025-03-28 22:24:32 +0700

    improve ui

--- app/components/app/AppSidebar.vue
+++ app/components/app/AppSidebar.vue
@@ -1,139 +1,180 @@
 <template>
-  <nav class="space-y-1">
-    <h1 class="text-3xl font-bold py-2 mb-2">Nostr Social</h1>
+  <div>
+    <nav class="space-y-1 flex-col md:flex hidden">
+      <h1 class="text-3xl font-bold hidden md:block py-2 mb-2">Nostr Social</h1>
 
-    <UButton
-      v-for="item in sidebarNavItems"
-      :key="item.label"
-      :icon="item.icon"
-      color="neutral"
-      variant="ghost"
-      block
-      class="justify-start"
-      :to="item.to"
-    >
-      {{ item.label }}
-    </UButton>
-
-    <UButton
-      v-if="user"
-      icon="i-heroicons-plus-circle"
-      color="primary"
-      block
-      to="/new-post"
-    >
-      Create Post
-    </UButton>
-
-    <!-- switch account -->
-    <UModal
-      :ui="{
-        content: 'w-full md:max-w-xs',
-      }"
-    >
-      <div v-if="user" class="flex mt-4 items-center gap-2 cursor-pointer">
-        <UAvatar :src="currentUserInfo?.picture" />
-        <span>
-          {{ currentUserInfo?.display_name || "Nostr User" }}
+      <UButton
+        v-for="item in sidebarNavItems"
+        :key="item.label"
+        :icon="item.icon"
+        color="neutral"
+        variant="ghost"
+        block
+        class="justify-start"
+        :to="item.to"
+      >
+        <span class="hidden md:block">
+          {{ item.label }}
         </span>
-      </div>
-      <div v-else>
-        <UButton
-          icon="i-heroicons-user"
-          variant="ghost"
-          class="w-full"
-          color="primary"
-        >
-          Login
-        </UButton>
-      </div>
-      <template #content>
-        <div>
-          <h1 class="text-lg px-4 py-2 font-bold">Switch Account</h1>
-          <ul class="p-1">
-            <li
-              v-for="(account, index) in accounts"
-              :key="index"
-              class="flex gap-4 items-center cursor-pointer rounded-md py-1 px-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              <span>
-                <UAvatar :src="currentUserInfo?.picture" />
-              </span>
-              <span class="flex-grow">
-                {{ account.display_name || "Nostr User" }}
-              </span>
-              <span v-if="index === 0">
-                <Icon name="i-heroicons-check" />
-              </span>
-            </li>
-            <li>
-              <UButton
-                icon="i-heroicons-plus-circle"
-                color="primary"
-                block
-                class="mt-2"
-                variant="ghost"
-              >
-                Add Account
-              </UButton>
-            </li>
-          </ul>
+      </UButton>
+
+      <UButton
+        v-if="user"
+        icon="i-heroicons-plus-circle"
+        color="primary"
+        to="/new-post"
+        block
+      >
+        Create Post
+      </UButton>
+
+      <!-- switch account -->
+      <section class="md:block hidden">
+        <div v-if="user" class="flex mt-4 items-center gap-2 cursor-pointer">
+          <UAvatar :src="currentUserInfo?.picture" />
+          <span>
+            {{ currentUserInfo?.display_name || "Nostr User" }}
+          </span>
         </div>
-      </template>
-    </UModal>
-  </nav>
+        <div v-else>
+          <UButton
+            icon="i-heroicons-user"
+            variant="ghost"
+            class="w-full"
+            color="primary"
+          >
+            Login
+          </UButton>
+        </div>
+      </section>
+
+      <UModal
+        v-model:open="switchAccountModal"
+        :ui="{
+          content: 'w-full md:max-w-xs',
+        }"
+      >
+        <template #content>
+          <div>
+            <h1 class="text-lg px-4 py-2 font-bold">Switch Account</h1>
+            <ul class="p-1">
+              <li
+                v-for="(account, index) in accounts"
+                :key="index"
+                class="flex gap-4 items-center cursor-pointer rounded-md py-1 px-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                <span>
+                  <UAvatar :src="currentUserInfo?.picture" />
+                </span>
+                <span class="flex-grow">
+                  {{ account.display_name || "Nostr User" }}
+                </span>
+                <span v-if="index === 0">
+                  <Icon name="i-heroicons-check" />
+                </span>
+              </li>
+              <li>
+                <UButton
+                  icon="i-heroicons-plus-circle"
+                  color="primary"
+                  block
+                  class="mt-2"
+                  variant="ghost"
+                >
+                  Add Account
+                </UButton>
+              </li>
+            </ul>
+          </div>
+        </template>
+      </UModal>
+    </nav>
+
+    <nav
+      class="md:hidden z-10 fixed bottom-0 bg-white w-full items-center justify-between flex"
+    >
+      <UButton
+        v-for="item in itemForMobile"
+        :key="item.label"
+        :icon="item.icon"
+        color="neutral"
+        variant="ghost"
+        block
+        size="xl"
+        :to="item.to"
+      >
+        <span class="hidden md:block">
+          {{ item.label }}
+        </span>
+      </UButton>
+    </nav>
+  </div>
 </template>
 
 <script setup lang="ts">
 // Sidebar Navigation Items
 const { user, currentUserInfo } = useNostr();
+const switchAccountModal = ref(false);
 const sidebarNavItems = computed(() => [
   {
     label: "Home",
     icon: "i-heroicons-home",
     to: "/",
+    isMobile: true,
   },
   {
     label: "Shorts",
     icon: "simple-icons:youtubeshorts",
     to: "/shorts",
+    isMobile: true,
   },
   {
     label: "Search",
     icon: "system-uicons:search",
     to: "/search",
+    isMobile: true,
   },
   {
     label: "Messages",
     icon: "i-heroicons-chat-bubble-left-right",
     to: "/messages",
+    isMobile: true,
   },
   {
     label: "Journals",
     icon: "i-heroicons-book-open",
     to: "/journals",
+    isMobile: false,
   },
   {
     label: "Bookmarks",
     icon: "i-heroicons-bookmark",
     to: "/bookmarks",
+    class: "hidden md:block",
+    isMobile: false,
   },
   {
     label: "Notifications",
     icon: "i-heroicons-bell",
     to: "/notifications",
+    isMobile: true,
   },
   {
     label: "Profile",
     icon: "i-heroicons-user-circle",
     to: `/profile/${user.value?.npub}`,
+    class: "hidden md:block",
+    isMobile: true,
   },
   {
     label: "Settings",
     icon: "i-heroicons-cog",
     to: "/settings",
+    isMobile: false,
   },
 ]);
+
+const itemForMobile = sidebarNavItems.value.filter((item) => item.isMobile);
 
 const accounts = computed(() =>
   [
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,28 @@
+<script setup lang="ts">
+const { user } = useNostr();
+</script>
+
 <template>
-  <div class="flex divide-x divide-slate-100 h-full container mx-auto">
-    <aside class="px-4 sticky top-0 h-screen w-60">
+  <div
+    class="flex h-full w-full flex-col-reverse md:flex-row md:divide-x divide-slate-100 container mx-auto"
+  >
+    <aside
+      class="md:px-4 md:sticky md:top-0 bottom-0 md:h-screen w-full md:w-60"
+    >
       <AppSidebar class="" />
     </aside>
     <main class="flex-1 flex flex-col">
+      <UButton
+        v-if="user"
+        icon="i-heroicons-plus-circle"
+        color="primary"
+        to="/new-post"
+        square
+        size="xl"
+        class="fixed w-10 h-10 shadow-lg flex items-center rounded-full justify-center bottom-10 right-4 md:hidden"
+      >
+      </UButton>
+
       <slot />
     </main>
   </div>
