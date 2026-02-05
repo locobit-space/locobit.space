2b517f656b41a202a91aebcf322be722f1933e61
Mark AI <chanthavong_12@hotmail.com>
2026-02-05 09:27:32 +0700

    improve config pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -140,6 +140,57 @@ export default defineNuxtConfig({
         },
       ],
     },
+    workbox: {
+      navigateFallback: "/",
+      globPatterns: ["**/*.{js,css,html,png,svg,ico,woff2,woff,ttf}"],
+      globIgnores: ["**/node_modules/**/*", "sw.js", "workbox-*.js"],
+      cleanupOutdatedCaches: true,
+      navigateFallbackDenylist: [/^\/_payload\.json$/],
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+          handler: "CacheFirst",
+          options: {
+            cacheName: "google-fonts-cache",
+            expiration: {
+              maxEntries: 10,
+              maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+            },
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
+          },
+        },
+        {
+          urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+          handler: "CacheFirst",
+          options: {
+            cacheName: "gstatic-fonts-cache",
+            expiration: {
+              maxEntries: 10,
+              maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+            },
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
+          },
+        },
+        {
+          urlPattern: "/*",
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "nuxt-app-cache",
+            expiration: {
+              maxEntries: 50,
+              maxAgeSeconds: 60 * 60 * 24 * 1, // <== 1 day
+            },
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
+          },
+        },
+      ],
+    },
     devOptions: {
       enabled: true,
       type: "module",
